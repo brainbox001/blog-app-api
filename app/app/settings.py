da9c48da97db1e2298854a30e08783fbe6c379e0
Marcus Daniel <momsdboy@gmail.com>
2024-02-01 14:42:06 +0100

    Revert "settings modified, ckeditor added, collectstatic ran"
    This reverts commit 6c2a3a5f84e80e7714b03810a65612315a191571.

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_celery_results',
-    'ckeditor',
-    'ckeditor_uploader',
     'core',
     'user',
     'rest_framework',
@@ -139,8 +137,6 @@ MEDIA_URL = '/static/media/'
 
 MEDIA_ROOT = 'vol/web/media'
 STATIC_ROOT = 'vol/web/static'
-
-CKEDITOR_UPLOAD_PATH = "uploads/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
